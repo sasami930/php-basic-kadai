13d8d6d729df0d28aef6933c9fc688edffb6e906
sasami930 <m15863480@gmail.com>
2024-12-04 22:31:48 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,22 @@
+<?php
+
+$scole1 = 80;
+$scole2 = 60;
+$scole3 = 55;
+$scole4 = 40;
+$scole5 = 100;
+$scole6 = 25;
+$scole7 = 80;
+$scole8 = 95;
+$scole9 = 30;
+$scole10 = 60;
+
+$total = $scole1 + $scole2 + $scole3 + $scole4 + $scole5 + $scole6 + $scole7 + $scole8 + $scole9 + $scole10;
+
+echo "合計点：" .$total . "<br>";
+
+$average = $total / 10;
+
+echo "平均点：" . $average;
+
+?>
