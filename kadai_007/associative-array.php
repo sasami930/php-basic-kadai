d8bbe2e032c4af6a9d81274178811517d0eda73e
sasami930 <m15863480@gmail.com>
2024-12-10 00:05:41 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列</title>
+</head>
+<body>
+  
+  <?php
+
+  $assosiative = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+
+  print_r($assosiative);
+
+  ?>
+
+
+
+</body>
+</html>
